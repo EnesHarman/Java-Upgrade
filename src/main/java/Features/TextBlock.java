003656c7334a34f3043b9d54dcf8a6a53ab430f0
EnesHarman <enesharman60@gmail.com>
2023-05-21 10:38:39 +0300

    Add TextBlocks

---
+++ src/main/java/Features/TextBlock.java
@@ -0,0 +1,15 @@
+package Features;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        var json = """
+                {
+                    "name":"Enes",
+                    "surname":"Harman",
+                    "age":23,
+                    "job":"Software Engineer"
+                }
+                """;
+        System.out.println(json); // <3
+    }
+}
